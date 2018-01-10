62f9d3678c0f285d8fb5e1e9c69cf9b1f6d7ea69
Cavicck <vittorio.cavicchioli@gmail.com>
2018-01-10 19:55:49 +0100

    Aggiungo sto schifo di index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head></head>
+<body>
+<!-- INIZIO FORM ARTISTI-->
+<form method="post">
+Name: <input type="text"><br>
+Gender: <input type="radio" name="gender" value="male">	<input type="radio" name="gender" value="female"><br>
+Anno nascita/morte (date): <input type="number" min="0" max ="2018">
+Year of birth: <input type="number" min="0" max="2018"><br>
+Year of death: <input type="number" min="0" max="2018"><br>
+</form><br>
+<!-- INIZIO FORM OPERE-->
+<form method="post">
+Artista:
+Ruolo artista:
+Titolo:
+Data creazione:
+Metodo di creazione:
+credit line(??):
+Anno:
+Anno acquisizione:
+Dimensioni:
+Larghezza:
+Altezza:
+Spessore (depth):
+Unità di misura:
+Incisione(?):
+Copyright copertina/illustrazione/thumbnail:
+</form>
+<?php 
+
+?>
+</body>
+</html>
