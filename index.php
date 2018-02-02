6ac43a752b3342aca56ecaeaab2760f601774e86
Dmitry Babich <d.babich@yahoo.com>
2018-02-02 23:21:13 +0100

    Messo il logo

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<html>
+<html lang="en">
+
 
 	<head>
+		<meta charset="utf-8">
+    		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
@@ -11,6 +15,9 @@
 	</head>
 <body>
 <!-- INIZIO FORM ARTISTI-->
+
+<h1> Yooo Madafaka </h1>
+<!-- JAVAFX WRAPPER -->
 <form method="post">
 Name: <input type="text"><br>
 Gender: <input type="radio" name="gender" value="male">	<input type="radio" name="gender" value="female"><br>
@@ -39,5 +46,11 @@ Copyright copertina/illustrazione/thumbnail:
 <?php 
 
 ?>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
