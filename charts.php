d1029e7ab12e8e0e6aee225ec4a691b90972bb64
dmitrybbch <d.babich@yahoo.com>
2018-07-29 13:56:17 +0200

    Un cazzo di grafico che funzia, finalmente

--- charts.php
+++ charts.php
@@ -16,6 +16,8 @@
   <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin.css" rel="stylesheet">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
   
 </head>
 
@@ -91,19 +93,107 @@
             <div class="card-header">
               <i class="fa fa-bar-chart"></i> Bar Chart Example</div>
             <div class="card-body">
-              <canvas id="myBarChart" width="100" height="50"></canvas>
+              <!--<canvas id="myBarChart" width="100" height="50"></canvas>-->
+                <div id="chart-container">
+                    <canvas id="graphCanvas"></canvas>
+                </div>
+                <script>
+                    $(document).ready(function () {
+                        showGraph();
+                    });
+
+
+                    function showGraph()
+                    {
+                        {
+                            $.post("premadeQueries.php",
+                                function (data)
+                                {
+                                    console.log(data);
+                                    var name = [];
+                                    var marks = [];
+
+                                    for (var i in data) {
+                                        name.push(data[i].year);
+                                        marks.push(data[i].conto_opere);
+                                        console.log(data[i].year + " " + data[i].conto_opere);
+                                    }
+
+                                    var chartdata = {
+                                        labels: name,
+                                        datasets: [
+                                            {
+                                                label: 'Works per year',
+                                                backgroundColor: '#49e2ff',
+                                                borderColor: '#46d5f1',
+                                                hoverBackgroundColor: '#CCCCCC',
+                                                hoverBorderColor: '#666666',
+                                                data: marks
+                                            }
+                                        ]
+                                    };
+
+                                    var graphTarget = $("#graphCanvas");
+
+                                    var barGraph = new Chart(graphTarget, {
+                                        type: 'bar',
+                                        data: chartdata
+                                    });
+                                });
+                        }
+                    }
+                </script>
             </div>
-            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+
+            <!-- MENU A CASCATA -->
+              <form action="worksPerYear.php" method="post" id="barChartQueryForm">
+                  <div class="form-group">
+                      <label for="inputQueryBarChart">Select the data to show:</label>
+                      <select class="form-control" id="inputQueryBarChart" onchange="this.form.submit()">
+                          <option value="worksperyear">Works Per Year</option>
+                          <option value="query1">Query 1</option>
+                          <option value="query2">Query 2</option>
+                          <option value="query3">Query 3</option>
+                          <option value="query4">Query 4</option>
+                      </select>
+                  </div>
+                  <div class="form-group">
+                      <table>
+                          <tr class="form-group">
+                              <td><label for="birthyear"><strong>Anno di nascita: </strong></label></td>
+                              <td><input type="text" name="birthyear" class="form-control" id="birthyear" placeholder="Anno di nascita dell'artista" onkeypress='return event.charCode >= 48 && event.charCode <= 57'></td>
+                          </tr>
+                          <tr>
+                              <td><label for="deatyear"><strong>Anno di morte: </strong></label></td>
+                              <td><input type="text" name="deathyear" class="form-control" id="deathyear" placeholder="Anno di nascita dell'artista" onkeypress='return event.charCode >= 48 && event.charCode <= 57'></td>
+                          </tr>
+                          <tr>
+                              <td><label for="gender"><strong>Sesso: </strong></label></td>
+                              <td><input type="text" name="gender" class="form-control" id="gender"></td>
+                          </tr>
+                          <tr>
+                              <td><label for="birthplace"><strong>Luogo di nascita: </strong></label></td>
+                              <td><input type="text" name="birthplace" class="form-control" id="birthplace"></td>
+                          </tr>
+                          <tr>
+                              <td><label for="deathplace"><strong>Luogo di morte: </strong></label></td>
+                              <td><input type="text" name="deathplace" class="form-control" id="deathplace"></td>
+                          </tr>
+                      </table>
+                  </div>
+              </form>
+              <!-- /MENU A CASCATA-->
           </div>
         </div>
         <div class="col-lg-4">
           <!-- Example Pie Chart Card-->
           <div class="card mb-3">
             <div class="card-header">
-              <i class="fa fa-pie-chart"></i> Pie Chart Example</div>
+              <i class="fa fa-pie-chart"></i> Sesso degli artisti</div>
             <div class="card-body">
-              <canvas id="myPieChart" width="100%" height="100"></canvas>
+              <canvas id="pieChart" width="100%" height="100"></canvas>
             </div>
+
             <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
           </div>
         </div>
---
+++ worksPerYear.php
@@ -0,0 +1,24 @@
+<?php
+
+/*verifica della presenza di errori
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+
+header('Content-Type: application/json');
+$conn = mysqli_connect("localhost","pippo","topolino","musei");
+
+$sqlQuery = "SELECT COUNT(*) as conto_opere, year FROM artwork GROUP BY year";
+
+$result = mysqli_query($conn,$sqlQuery);
+$anniCount = array();
+$dataC = array();
+foreach ($result as $row) {
+    $data[] = $row;
+}
+
+mysqli_close($conn);
+
+echo json_encode($data);
+
